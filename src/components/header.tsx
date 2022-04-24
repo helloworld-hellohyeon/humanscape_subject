2a01f9cda241560ed6b276cc7824fa41ed44fc43
HYEON <helloworld.hellohyeon@gmail.com>
2022-04-24 16:54:33 +0900

    components 위치 수정

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,11 @@
 import { memo } from "react";
 import styled from "@emotion/styled";
 
-const Self = styled.header``;
+const Self = styled.header`
+  width: 100%;
+  height: 56px;
+  background-color: #fff;
+`;
 
 const Header = () => {
   return <Self></Self>;
